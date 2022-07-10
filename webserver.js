3b31480d40354643923ad3f5ff991a449722ab95
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-07-10 08:00:20 +0200

    added tutorials page, updated index.html and deleted useless thing from webserver.js

--- webserver.js
+++ webserver.js
@@ -1,20 +1,12 @@
 var server = require('express')();
 var config = require('./config.json');
 
-
-
-
-
-const parseIp = (request) =>
-       request.headers['x-forwarded-for']?.split(',').shift()
-    || request.socket?.remoteAddress
-
-
-    server.get('/', function(request, response){
-    console.log(parseIp(request), '- Got the session')
+server.get('/', function(request, response){
     response.sendFile(__dirname + '/public/index.html');
 });
+server.get('/poradniki', function(request, response){
+    response.sendFile(__dirname + '/public/poradniki.html');
+});
 
-
-server.listen(config.port)
+server.listen(config.port);
 console.log('Server started on port:' , config.port)
